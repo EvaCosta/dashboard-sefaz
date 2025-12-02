90157d7c4486227e59ebc7cbc6b4b6aa71d8c7c7
evacosta <evacosta.if@gmail.com>
2025-12-02 09:09:59 -0300

    exercicios

---
+++ src/exercicios/exercicio.js
@@ -0,0 +1,14 @@
+// Exercício 1: O Objeto Solitário (A Ficha)
+// Objetivo: Entender chave/valor. 
+// Comando: "Rafa, cria uma variável que descreve VOCÊ. 
+// Quero nome, idade e se está empregada."
+
+// Exercício 2: A Lista Simples (O Post-it)
+// Objetivo: Entender índice numérico. Comando: "Agora esquece ficha. 
+// Faz uma lista de compras de mercado simples. Só texto."
+
+// Exercício 3: A Fusão
+// Objetivo: Juntar os dois conceitos. 
+// Cenário: "Vamos criar o sistema do Spotify. 
+// A gente precisa de uma lista de músicas. 
+// Cada música tem Título e Artista."
